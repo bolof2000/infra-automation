94255dc4e0948b1cae8333844349c2c41484d54a
Victor Bolofinde <bolofindeolusegun@yahoo.com>
2021-09-19 23:29:02 -0600

    another bucket

--- s3.tf
+++ s3.tf
@@ -2,3 +2,7 @@
 resource "aws_s3_bucket" "spring-boot-deployment" {
     bucket = "spring-boot-bolof-test-005"
 }
+resource "aws_s3_bucket" "bolof-aws-bucket-2" {
+
+    bucket = "bolof-bukcet-spring-boot-001"
+}
