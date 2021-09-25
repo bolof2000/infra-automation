52645ca2c49c794372c1aa18b8ca73736738ebd0
Victor Bolofinde <bolofindeolusegun@yahoo.com>
2021-09-25 10:32:55 -0600

    done

--- aws-resource.tf
+++ aws-resource.tf
@@ -1,10 +1,5 @@
 
-provider "aws" {
-    access_key = "AKIAWKSTNSOWYZTHPZJ4"
-    secret_key = "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"
-    region = "us-east-1"
 
-}
 
 resource "aws_instance" "drone-ci" {
     ami = "ami-09e67e426f25ce0d7"
